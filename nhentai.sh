eb7d0db979947c347cf3b702909c5967a0910e35
rustyinfinity <115462641+rustyinfinity@users.noreply.github.com>
2023-12-21 04:23:08 +0530

    Added Bulk Downloading and much more !
    1) Added Bulk Downloading ( Not Parallel ! * Future Plan )
    2) Improved Interface
    3) Added error messages on breaking 
    4) Overall optimised script

--- nhentai.sh
+++ nhentai.sh
@@ -1,39 +1,19 @@
 #!/bin/bash
 
 ############### PREQUISTE ###############
-
 CONFIG_PATH="./main.config"
+LINKS_PATH="./links.txt"
 source $CONFIG_PATH
 
 if [ -z $DOWNLOAD_DIR ]
 then
-DOWNLOAD_DIR="./"
+DOWNLOAD_DIR="."
 fi
 
-############### INPUT ###############
-
-echo "1) Nhentai
-2) Hentaifox.com
-3) E-Hentai.org (Enter Full URL !)
-4) 3Hentai.net
-5) Pururin.to
-6) Asmhentai.com
-7) Hentai2read.com (Enter Full URL !)"
-echo -n "-> "
-read website
-
-if [ $website == 1 ]
+if [ -z $LINKS_PATH ]
 then
-echo "1) nhentai.net (Make Sure You Added cf_clearance and csrftoken in config file ! ie $CONFIG_PATH)
-2) nhentai.to"
-
-echo -n "-> "
-read nhentai_domain
+touch "./links.txt"
 fi
-
-echo "Enter ID or URL"
-echo -n "-> "
-read input
 
 ############### HEADERS ###############
 
@@ -44,16 +24,6 @@ header_4="cookie: cf_clearance=${cf_clearance}; csrftoken=${csrftoken}"
 header_5="authority: nhentai.net"
 h2r_header="referer: https://hentai2read.com/"
 
-############### ID ###############
-
-id=$(echo "$input" | grep -Eo '[0-9]*')
-
-############### TEMP ###############
-
-mkdir -p "./.temp" && rm -rf ./.temp/*
-
-declare -a links
-
 ############### FUNCTIONS ###############
 
 default_downloader(){
@@ -62,10 +32,59 @@ default_downloader(){
    echo "Downloaded !"
 }
 
+bulk_download_check(){
+clear
+echo -e "(1) Download Single File \n(2) Download in Bulk ( Make Sure you have ${LINKS_PATH} file! )"
+echo -en "\n-> "
+read download_choice
+
+if [ $download_choice == 1 ]
+then
+   clear
+   links=()
+   mkdir -p "./.temp" && rm -rf ./.temp/*
+   echo "Enter ID or URL"
+   echo -en "\n-> "
+   read input
+   id=$(echo "$input" | grep -Eo '[0-9]*')
+   $(echo $site_name) && worker
+elif [ $download_choice == 2 ]
+then
+   clear
+   bulk_download
+else
+   clear
+   echo "Please Choose a Valid Option!"
+   exit
+fi
+}
+
+bulk_download(){
+totalLinks=$(cat ${LINKS_PATH} | sed '/^[[:space:]]*$/d' | wc -l)
+if [ $totalLinks == 0 ]
+then
+clear
+echo "No Links in ${LINKS_PATH} exiting ..."
+exit
+fi
+echo "Total Links Found: $totalLinks"
+for (( m=1; m<=$totalLinks; m++ ));
+do
+   links=()
+   mkdir -p "./.temp" && rm -rf ./.temp/*
+   input=$(cat ${LINKS_PATH} | sed -n $m\p)
+   id=$(echo $(cat ${LINKS_PATH} | sed -n $m\p) | grep -Eo '[0-9]*' | head -n 1)
+   echo "Downloading ${id}"
+   $(echo $site_name) && worker
+done
+}
+
 #-O "./.temp/${i}.${ext}"
 
+
 convert_cleanup(){
-zip -r "${DOWNLOAD_DIR}/${id}_${site_name}.cbz"  ./.temp/* && rm -rf ./.temp
+filename=$(echo "$(echo "$html" | grep -o '<title>.*</title>' | sed 's/<title>//g' | sed 's/<\/title>//g')_${id}_${site_name}.cbz" | sed 's/\///g')
+zip  "${DOWNLOAD_DIR}/${filename}"  ./.temp/* && rm -rf ./.temp
 }
 
 summary(){
@@ -74,16 +93,37 @@ echo "Total Pages :- $total_pages"
 echo "Hentai ID :- $id"
 }
 
-
-nhentai(){
-
-if [ $nhentai_domain == 1 ]
+pages_error(){
+if [ -z ${total_pages} ]
 then
+clear
+echo "ERROR: TOTAL PAGES NULL! "
+exit
+fi
+}
+images_error(){
+if [ -z ${image_link} ]
+then
+clear
+echo "ERROR: IMAGE LINK NULL! "
+exit
+fi
+}
+
+Nhentai.net(){
+if [ -z ${cf_clearance} ] && [ -z ${csrftoke} ]
+then
+clear
+echo "ERROR: EMPTY cf_clearance & csrftoken in config File please add or try another Website!"
+exit
+fi
 url="https://nhentai.net/g/$id/"
 html=$(curl -s "$url" -H "${header_5}" -H "${header_1}" -H "${header_4}" -H "${header_3}" -H "${header_2}" )
 total_pages=$(echo "$html" | grep -Eo '<span class="name">[0-9]*</span>' | grep -Eo '[0-9]*')
 sub_link="${url}${i}/"
 image_link=$(curl -s "${sub_link}" -H "${header_5}" -H "${header_1}" -H "${header_4}" -H "${header_3}" -H "${header_2}" | grep -Eo 'https://[a-zA-Z0-9?%-_]*/galleries/[0-9]*/[0-9]*.[a-z]*' )
+pages_error
+images_error
 for (( i=1 ; i<="$total_pages" ; i++ ));
 do
 sub_link="${url}${i}/"
@@ -93,14 +133,13 @@ links+=([$i]="${image_link}")
 echo -n "#"
 done
 echo " "
-
-elif [ $nhentai_domain == 2 ]
-then
+}
+Nhentai.to(){
 url="https://nhentai.to/g/$id"
 header_3="referer: https://nhentai.to/"
 html=$(curl -s "$url" -H "${header_1}" -H "${header_2}" -H "${header_3}"  )
 total_pages=$(echo "$html" | grep -Eo '<span class="name">[0-9]*</span>' | grep -Eo '[0-9]*')
-
+pages_error
 for (( i=1 ; i<="$total_pages" ; i++ ));
 do
 sub_link="${url}/${i}"
@@ -110,33 +149,20 @@ links+=([$i]="${image_link}")
 echo -n "#"
 done
 echo " "
-else 
-exit
-fi
-
-
 }
+
 
 hentaifox(){
    url="https://hentaifox.com/gallery/$id/"
    header_3="referer: https://hentaifox.com/"
-   html=$(curl -s "$url" -H "${header_1}" -H "${header_2}" -H "${header_3}" )
+   html=$(curl -s "$url" -H "${header_2}" )
    total_pages=$(echo "$html" | grep -Eo 'Pages: [0-9]*' | sed 's/Pages: //g')
-   image_domain=$(echo "$html" | grep -Eo 'https://[a-zA-Z0-9?%-_]*/[0-9]*/[0-9]*/[a-zA-Z0-9?%-_]*cover.jpg' | grep -Eo 'https://i.hentaifox.com/[0-9]*/[0-9]*')
-   
-   
-
+   pages_error
    for (( i=1 ; i<="$total_pages" ; i++ ));
    do
-   is_png=$(curl -s "$image_domain/$i.png" -H $header_1 -H $header_2 -H $header_3  | grep -Eo 'Not Found' | head -n 1)
-   if [[ -z "$is_png" ]]; 
-   then
-   ext="png"
-   else
-   ext="jpg"
-   fi
+   image_url=$(curl -s "https://hentaifox.com/g/${id}/${i}/" -H "${header_2}" | grep -Eo 'data-src="https://.*[0-9]*\.[a-z]*"' | sed 's/data-src=//g' | sed 's/"//g')
    links+=(
-      [$i]="${image_domain}/${i}.${ext}"
+      [$i]=${image_url}
    )
    echo -n "#"
    done
@@ -149,8 +175,7 @@ e-hentai(){
    html=$(curl  -s "$url")
    total_pages=$(echo "$html" | grep -Eo 'https://e-hentai.org/s/[a-zA-Z0-9?%-_]*/[a-zA-Z0-9?%-_]*"><img alt=' | sed 's/"><img alt=//g' | wc -l )
    sub_page_links=$(echo "$html" | grep -Eo 'https://e-hentai.org/s/[a-zA-Z0-9?%-_]*/[a-zA-Z0-9?%-_]*"><img alt=' | sed 's/"><img alt=//g' )
-   
-   
+   pages_error
 
    for (( i=1 ; i<=$total_pages ; i++ ));
    do
@@ -167,7 +192,7 @@ e-hentai(){
    links+=(
       [$i]="${link}","${i}.${ext}"
    )
-   
+
    echo -n  "#"
 
    done
@@ -182,33 +207,24 @@ e-hentai(){
    html=$(curl -s $url)
    sub_page_links=$(echo "$html" | grep -Eo 'https://3hentai.net/d/[0-9]*/[0-9]*')
    total_pages=$(echo "$html" | grep -Eo 'https://3hentai.net/d/[0-9]*/[0-9]*' | wc -l )
-
-
+   pages_error
    for (( i=1 ; i<=$total_pages ; i++ ));
    do
    link=$(curl -s $(echo "$sub_page_links" | sed -n $i\p ) | grep -Eo 'https://[a-zA-Z0-9?%-_]*/[a-zA-Z0-9]*/[0-9]*\.[a-z]*' )
-   is_png=$(echo "$link" | grep -Eo "\.png" )
-   if [[ -z $is_png ]]; 
-   then
-   ext="jpg"
-   else
-   ext="png"
-   fi
-
-   links+=([$i]="$link")
+   links+=([$i]=${link})
    echo -n "#"
    done
-   
    echo " "
 
 }
 
 Pururin(){
+   id=$(echo "${input}" | grep -Eo 'gallery/.[0-9]*' | sed 's/gallery\///g')
    url="https://pururin.to/gallery/$id"
    html=$(curl -s "${url}" -H "${header_1}" -H "${header_2}" -H "${header_3}" )
    total_pages=$(echo "$html" | grep -Eo '"numberOfPages">[0-9]*' | grep -Eo '[0-9]*')
    sub_page_links=$(echo "$html" | grep -Eo 'https://pururin.to/read/[a-zA-Z0-9?%-_/]*' | awk '!seen[$0]++' )
-
+   pages_error
    for (( i=1 ; i<=$(echo "$sub_page_links" | wc -l ) ; i++ ));
    do
    link=$(curl -s $(echo "$sub_page_links" | sed -n $i\p ) | grep -Eo 'https://i.pururin.co/[0-9]*/[0-9]*.[a-z]*' | awk '!seen[$0]++' )
@@ -224,6 +240,7 @@ Asmhentai(){
    url="https://asmhentai.com/g/$id/"
    html=$(curl -s $url )
    total_pages=$(echo "$html" |  grep -Eo 'Pages: [0-9]*' | grep -Eo '[0-9]*')
+   pages_error
    for (( i=1 ; i<=$total_pages ; i++ ));
    do
    image_link=$(curl -s $(echo "${url}${i}/" | sed 's/g/gallery/g') | grep -Eo "https://images.asmhentai.com/[0-9]*/[0-9]*/[a-z0-9?%-_]*" )
@@ -241,6 +258,7 @@ url="$input"
 html=$(curl -s $url)
 id=$(echo "$url" | sed 's/https:\/\/hentai2read.com\///g' | sed 's/\///g' )
 total_pages=$(echo "$html" | grep -Eo '[0-9]* pages' | grep -Eo '[0-9]*' )
+pages_error
 for (( i=1 ; i<=$total_pages ; i++ ));
 do
 image_link=$(curl -s "${url}1/${i}/" | grep -Eo 'https://static.hentai.direct/hentai/[0-9]*/[0-9]*/[a-zA-Z0-9?%-_]*' )
@@ -257,62 +275,73 @@ echo "Downloaded !"
 
 }
 
+worker(){
+default_downloader && convert_cleanup && summary
+}
+
+
+############### NHLOGO ###############
+logo(){
+echo "CiAgXyAgXyBfICBfICAgIF8gICAgICAgICAgICAgICAgIF8gICAgICAgICAgICAgIF8gICAgICAgICAKIHwgXHwgfCB8fCB8X198IHxfX19fXyBfXyBfX18gXyB8IHxfX18gIF9fIF8gX198IHxfX18gXyBfIAogfCAuYCB8IF9fIC8gX2AgLyBfIFwgViAgViAvICcgXHwgLyBfIFwvIF9gIC8gX2AgLyAtXykgJ198CiB8X3xcX3xffHxfXF9fLF9cX19fL1xfL1xfL3xffHxffF9cX19fL1xfXyxfXF9fLF9cX19ffF98ICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAo="  | base64 -d 
+}
+############### INPUT ###############
+
+choose_website(){
+echo -e "(1) Nhentai.net
+(2) Hentaifox.com
+(3) E-Hentai.org ( Enter Full URL! )
+(4) 3Hentai.net
+(5) Pururin.to
+(6) Asmhentai.com
+(7) Hentai2read.com ( Enter Full URL! )
+(8) Nhentai.to"
+echo -en "\n-> "
+read website
+}
 
 ############### EXECUTION ###############
+logo && choose_website
 
 if [ $website == 1 ]
 then
-site_name="nhentai"
-nhentai
-default_downloader
+site_name="Nhentai.net"
+bulk_download_check
 #echo "${links[@]}"
-convert_cleanup
-summary
 
 elif [ $website == 2 ]
 then
 site_name="hentaifox"
-hentaifox
-default_downloader
-convert_cleanup
-summary
-
+bulk_download_check
 elif [ $website == 3 ]
 then
 site_name="e-hentai"
-e-hentai
+bulk_download_check
 #echo "${links[@]}"
-default_downloader
-convert_cleanup
-summary
+
 elif [ $website == 4 ]
 then
 site_name="3Hentai"
-3Hentai
-default_downloader
-convert_cleanup
-summary
+bulk_download_check
 elif [ $website == 5 ]
 then
 site_name="Pururin"
-Pururin
-default_downloader
-convert_cleanup
-summary
+bulk_download_check
 elif [ $website == 6 ]
 then
 site_name="Asmhentai"
-Asmhentai
-default_downloader
-convert_cleanup
-summary
+bulk_download_check
 elif [ $website == 7 ] 
 then
 site_name="Hentai2read"
-Hentai2read
-convert_cleanup
-summary
+echo "I will fix it later :( [ Rest are Working! ]"
+#bulk_download_check && convert_cleanup && summary
 #Custom Download config !
+elif [ $website == 8 ] 
+then
+site_name="Nhentai.to"
+bulk_download_check
 else
+clear
+echo "Please choose a valid option ! "
 exit
 fi
